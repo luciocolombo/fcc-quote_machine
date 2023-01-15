80ec16b6ce3a251428868fd4cb15d651f8662ffa
lucio <colombolucio@hotmail.com>
2023-01-15 19:24:13 +0100

    full solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect, React } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const [quote, setQuote] = useState({});
+   useEffect(() => {
+      getQuote(setQuote, quoteArray);
+   }, []);
+   const quoteArray = [
+      {
+         text: "It is not what you do for your children, but what you have taught them to do for themselves, that will make them successful human beings.",
+         author: "- Anne Frank",
+      },
+      { text: "Nothing is impossible, the word itself says, “I’m possible!", author: "- Roger Staubach" },
+      { text: "Remember that not getting what you want is sometimes a wonderful stroke of luck.", author: "- Henry David Thoreau" },
+   ];
+   return (
+      <div id="quote-box" className="App container">
+         <div className="container card p-5">
+            <h3 id="text">
+               <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="35"
+                  height="35"
+                  fill="currentColor"
+                  className="bi bi-quote"
+                  viewBox="0 0 16 16"
+               >
+                  <path d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 9 7.558V11a1 1 0 0 0 1 1h2Zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 3 7.558V11a1 1 0 0 0 1 1h2Z" />
+               </svg>
+               {quote.text}
+            </h3>
+            <h5 className="py-2" id="author">
+               {quote.author}
+            </h5>
+            <div id="share" className="d-flex justify-content-between pt-3 px-5">
+               <a href="twitter.com/intent/tweet" id="tweet-quote" className="btn btn-secondary ">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" className="bi bi-twitter" viewBox="0 0 16 16">
+                     <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
+                  </svg>
+               </a>
+               <button className="btn btn-secondary" id="new-quote" onClick={() => getQuote(setQuote, quoteArray)}>
+                  New Quote
+               </button>
+            </div>
+         </div>
+      </div>
+   );
+}
+
+function getQuote(setQuote, quoteArray) {
+   const selectedQuote = quoteArray[getNumber()];
+   setQuote({
+      text: selectedQuote.text,
+      author: selectedQuote.author,
+   });
+}
+function getNumber() {
+   return Math.floor(Math.random() * 3);
 }
 
 export default App;
